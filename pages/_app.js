eb27204dedead413d6956c23720861ebf923c98a
Chaz Leong <cleong@rc-cleong-mbp.local>
2020-11-01 20:49:35 -1000

    Completed video 2 of nextjs + strapi tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Header from '../components/Header'
+import { ThemeProvider } from 'emotion-theming'
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles'
+
+const theme = {
+    colors: {
+        primary: '#efefef'
+    }
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <GlobalStyles />
+                <Header />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default MyApp
